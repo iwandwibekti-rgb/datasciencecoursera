8e6cf60ea341205c1c0e6a73b03dcf72ba960805
iwandwibekti-rgb <iwandwibekti@gmail.com>
2026-01-19 04:56:27 +0700

    inital script for rank hospital

---
+++ rankhospital.R
@@ -0,0 +1,15 @@
+
+library(tidyverse)
+library(stringr)
+library(hablar)
+
+data.hospital <- read.csv("C:/Users/Acer/Desktop/hospital-data.csv")
+data.outcome <- read.csv("C:/Users/Acer/Desktop/outcome-of-care-measures.csv", colClasses = "character")
+
+
+### 3 Ranking hospitals by outcome in a state
+
+### 4 Ranking hospitals in all states
+
+
+
